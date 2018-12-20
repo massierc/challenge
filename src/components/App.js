9965c54671bccc1824431371c7b3186679ebfb75
massierc <massimo.ercolani28@gmail.com>
2018-12-20 16:05:38 +0100

    create Auto1 theme

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from '../store';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import theme from '../theme';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Cars from './Cars';
 import Car from './Car';
 import NotFound from './NotFound';
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    color: ${props => props.theme.colors.text};
+    font-family: ${props => props.theme.font.family};
+  }
+`;
+
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={Cars} />
-            <Route path="/:carId" component={Car} />
-            <Route component={NotFound} />
-          </Switch>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Switch>
+              <GlobalStyle />
+              <Route exact path="/" component={Cars} />
+              <Route path="/:carId" component={Car} />
+              <Route component={NotFound} />
+            </Switch>
+          </BrowserRouter>
+        </ThemeProvider>
       </Provider>
     );
   }
---
+++ src/theme.js
@@ -0,0 +1,38 @@
+const theme = {
+  colors: {
+    primary: '#EA7F28',
+    secondary: '#D37324',
+    text: '#4A4A4A',
+    grey: '#EDEDED'
+  },
+  font: {
+    family: 'Roboto, Helvetica, sans-serif',
+    size: {
+      h1: '32px',
+      h2: '18px',
+      subtitle: '18px',
+      p1: '14px',
+      p2: '12px'
+    },
+    weight: {
+      header: 'bold',
+      body: 'regular'
+    }
+  },
+  spacing: {
+    l: '24px',
+    m: '12px',
+    s: '8px'
+  },
+  layout: {
+    header: '80px',
+    footer: '80px',
+    wrapper: '800px',
+    button: {
+      width: '128px',
+      height: '32px'
+    }
+  }
+};
+
+export default theme;
