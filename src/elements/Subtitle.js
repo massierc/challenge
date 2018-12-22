4236560d1d114abb04b26c0d35ffe34c0e5c9400
massierc <massimo.ercolani28@gmail.com>
2018-12-22 13:02:01 +0100

    create Subtitle element

---
+++ src/elements/Subtitle.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Subtitle = styled.span`
+  font-size: ${props => props.theme.font.size.subtitle};
+  font-weight: ${props => props.theme.font.weight.main};
+`;
+
+export default Subtitle;
